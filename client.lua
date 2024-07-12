05b7e4891f581efd776799d2feff3f5dbf173594
Atlanta Scripts <118575044+atlantascripts@users.noreply.github.com>
2024-07-12 22:31:37 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,30 +8,30 @@
 
 -- DISCORD SERVER: https://discord.gg/sCMKeZHxS5
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(100)
         if IsPedFalling(PlayerPedId()) or IsPedInParachuteFreeFall(PlayerPedId()) then
             SetEntityCanBeDamaged(PlayerPedId(), false)
         else
             SetEntityCanBeDamaged(PlayerPedId(), true)
         end
+        Wait(100)
     end
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(200)
         local Ped = GetPlayerPed(-1)
         SetPedCanRagdoll(Ped, false)
+        Wait(200)
     end
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(100)
         if IsPedGettingUp(PlayerPedId()) then
             ClearPedTasksImmediately(PlayerPedId(), true)
         end
+        Wait(100)
     end
 end)
